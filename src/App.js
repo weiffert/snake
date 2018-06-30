95b4f7b1427a4b4c8703debf808005308b0855a1
weiffert <weiffert@gmail.com>
2018-06-30 17:31:27 -0500

    What to do

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import "./App.css";
 
 import Board from "./Board";
 
+/* 
+ * TODO: 
+ * fix death screen.
+ * customize size to fit screen.
+ * make rectangles square.
+ * make a scoring system.
+ *  store high scores locally.
+ * customize stye of play.
+ *  speed
+ *  wrap around
+ * comment
+*/
+
 class App extends Component {
   render() {
     return (
