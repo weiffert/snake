8f061389eba253354dbf2ff3f5cbc73ce88d402d
weiffert <weiffert@gmail.com>
2018-07-28 14:46:56 -0500

    Style Splash screen

--- src/Splash.js
+++ src/Splash.js
@@ -1,27 +1,35 @@
 import React from "react";
 
+import "./Splash.css";
+
 class Splash extends React.Component {
   render = () => {
     return (
       <div className="Splash">
-        <form onChange={this.props.updateForm} onSubmit={this.props.start}>
-          <label for="speed">Speed</label>
-          <input
-            type="range"
-            id="speed"
-            min="50"
-            max="1000"
-            step="25"
-            value={this.props.speed}
-          />
-          <input
-            type="checkbox"
-            id="wrap"
-            value={this.props.wrap ? "checked" : "unchecked"}
-          />
-          <label for="wrap">Enable wrap around</label>
-        </form>
-        <h1>Press any key to start</h1>
+        <div>
+          <form onChange={this.props.updateForm} onSubmit={this.props.start}>
+            <div>
+              <label for="speed">Speed</label>
+              <input
+                type="range"
+                id="speed"
+                min="50"
+                max="300"
+                step="5"
+                value={this.props.speed}
+              />
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="wrap"
+                value={this.props.wrap ? "checked" : "unchecked"}
+              />
+              <label for="wrap">Enable wrap around</label>
+            </div>
+          </form>
+          <h1>Press any key to start</h1>
+        </div>
       </div>
     );
   };
